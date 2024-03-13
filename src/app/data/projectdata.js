85289264e83cd2e2a860f540b0fe71687e8d2888
tom-mate-o <132936694+tom-mate-o@users.noreply.github.com>
2024-03-13 07:49:50 +0100

    projects

--- src/app/data/projectdata.js
+++ src/app/data/projectdata.js
@@ -4,46 +4,101 @@ import projects from '../../img/home_item_images/projects.jpg';
 import photos from '../../img/home_item_images/photos.jpg';
 import art from '../../img/home_item_images/art.jpg';
 
-export const homedata = [
+export const projectdata = [
   {
     id: 1,
-    class: 'light',
-    name: 'About Me',
+
+    name: 'React',
+    tags: ['All', 'Web-App', 'React', 'JavaScript', 'Express', 'Node'],
     text: 'Coming from the high mountains of Austria, I washed ashore on an island in the Dutch North Sea. Not only did I switch my Apfelstrudel for Appeltaart, but I also switched my profession from being a metrologist in the automotive industry to following my passion for creating awesome things on the Web.',
-    image: about,
+    images: [
+      {
+        original: 'https://picsum.photos/id/1018/1000/600/',
+      },
+      {
+        original: 'https://picsum.photos/id/1015/1000/600/',
+      },
+      {
+        original: 'https://picsum.photos/id/1019/1000/600/',
+      },
+    ],
     linkto: '/aboutme',
   },
   {
     id: 2,
-    class: 'dark',
-    name: 'My Skills',
+
+    name: 'JavaScript',
+    tags: ['All', 'Web-App', 'JavaScript', 'SASS'],
     text: 'One thing hasn’t changed since I was a kid with a GameBoy and Pokémon Red. I always wanted to be the very best, and I trained hard for those gym badges.<br/>I’m proud to show you my badge collection regarding the Web Development League.',
 
-    image: skills,
+    images: [
+      {
+        original: 'https://picsum.photos/id/1018/1000/600/',
+      },
+      {
+        original: 'https://picsum.photos/id/1015/1000/600/',
+      },
+      {
+        original: 'https://picsum.photos/id/1019/1000/600/',
+      },
+    ],
     linkto: '/skills',
   },
   {
     id: 3,
-    class: 'light',
-    name: 'Projects',
+
+    name: 'Website',
+    tags: ['All', 'Website', 'JavaScript', 'SASS'],
     text: 'Here are some of my latest coding projects. From Websites to Full-Stack Web Apps. With screenshots and detailed information.<br/>Please have a look. And if you wish, you can take a deep dive and check their code on my Github page.',
-    image: projects,
+    images: [
+      {
+        original: 'https://picsum.photos/id/1018/1000/600/',
+      },
+      {
+        original: 'https://picsum.photos/id/1015/1000/600/',
+      },
+      {
+        original: 'https://picsum.photos/id/1019/1000/600/',
+      },
+    ],
     linkto: '/projects',
   },
   {
     id: 4,
-    class: 'dark',
-    name: 'Photos',
+
+    name: 'SASS Web App',
+    tags: ['All', 'Web-App', 'React', 'JavaScript', 'Mongo-DB', 'SASS'],
     text: 'Whenever I’m out and about, I love to capture the moment with my camera. Creating beautiful memories is one of my greatest desires.<br/>My shelves are filled with photobooks, just as my hard drive is filled with images. Come along; I’ll show you some of my best. ',
-    image: photos,
+    images: [
+      {
+        original: 'https://picsum.photos/id/1018/1000/600/',
+      },
+      {
+        original: 'https://picsum.photos/id/1015/1000/600/',
+      },
+      {
+        original: 'https://picsum.photos/id/1019/1000/600/',
+      },
+    ],
     linkto: '/photos',
   },
   {
     id: 5,
-    class: 'light',
-    name: 'Art',
+
+    name: 'Web App',
+    tags: ['All', 'Web-App', 'React', 'JavaScript', 'Node', 'Website'],
     text: 'I would not describe myself as an artist, but as creative person.<br/>I like to draw, scribble, and paint whenever I can.<br/>Click here to see some of my latest work.',
-    image: art,
+    images: [
+      {
+        original: 'https://picsum.photos/id/1018/1000/600/',
+      },
+      {
+        original: 'https://picsum.photos/id/1015/1000/600/',
+      },
+      {
+        original: 'https://picsum.photos/id/1019/1000/600/',
+      },
+    ],
     linkto: '/art',
   },
 ];
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import projectsBig from '../img/home_item_images/projects_big.jpg';
+import { projectdata } from '../app/data/projectdata';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import ImageGallery from 'react-image-gallery';
+import { ReactComponent as GitHubLink } from '../img/github.svg';
+import { ReactComponent as WebsiteLink } from '../img/eye-regular.svg';
 
 export default function Projects() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
+  const [selectedTags, setSelectedTags] = useState(['All']);
+  const [activeTag, setActiveTag] = useState('All');
+
+  const handleTagClick = (tag) => {
+    setSelectedTags([tag]);
+    setActiveTag(tag);
+  };
 
   return (
     <div>
@@ -19,7 +32,74 @@ export default function Projects() {
           <h1 className="singlepage__headerTitle">Projects</h1>
         </div>
         <div className="singlepage__content">
-          <p>Contact</p>
+          <div className="singlepage__filterbuttonsContainer">
+            {[
+              'All',
+              'React',
+              'Mongo-DB',
+              'Express',
+              'Node',
+              'SASS',
+              'JavaScript',
+              'Website',
+              'Web-App',
+            ].map((tag) => (
+              <button
+                className={`filterbutton ${tag} ${
+                  activeTag === tag ? 'active' : ''
+                }`}
+                onClick={() => handleTagClick(tag)}
+              >
+                {tag}
+              </button>
+            ))}
+          </div>
+
+          {projectdata
+            .filter((item) =>
+              item.tags.some((tag) => selectedTags.includes(tag))
+            )
+            .map((item) => (
+              <div className="projects__contentContainer">
+                <div className="projects__item" key={item.id}>
+                  <div className="text">
+                    <h1>{item.name}</h1>
+                    <p dangerouslySetInnerHTML={{ __html: item.text }} />
+                  </div>
+                  <div className="image">
+                    <ImageGallery
+                      class="imageGallery"
+                      items={item.images}
+                      showFullscreenButton={false}
+                      showBullets={true}
+                      showPlayButton={false}
+                      showThumbnails={false}
+                      slideDuration={500}
+                      slideInterval={2000}
+                      emulateTouch={true}
+                    />
+                  </div>
+                  <div className="buttons">
+                    <div className="morebutton">...more!</div>
+                    <div className="svgbuttons">
+                      <Link to="/" class="icon github">
+                        <GitHubLink class="" />
+                      </Link>
+                      <Link to="/" class="icon website">
+                        <WebsiteLink class="" />
+                      </Link>
+                    </div>
+                  </div>
+                  <div className="tags">
+                    {item.tags.map((tag) => (
+                      <div className={`tagbox ${tag}`}>
+                        <p>{tag}</p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            ))}
         </div>
       </div>
     </div>
