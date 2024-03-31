908723d3131944fe7e1c4570e21a98530da78761
tom-mate-o <132936694+tom-mate-o@users.noreply.github.com>
2024-03-31 09:45:20 +0200

    update texts

--- src/app/data/projectdata.js
+++ src/app/data/projectdata.js
@@ -26,6 +26,9 @@ import imgpokedex3 from '../../img/projects_images/pokedex/pokedex3.jpeg';
 import imgplanetfacts1 from '../../img/projects_images/planetfacts/pf1.jpeg';
 import imgplanetfacts2 from '../../img/projects_images/planetfacts/pf2.jpeg';
 import imgplanetfacts3 from '../../img/projects_images/planetfacts/pf3.jpeg';
+import xdream from '../../img/projects_images/xdream/xdream.jpeg';
+import radioquiz1 from '../../img/projects_images/radioquiz/radioquiz1.jpeg';
+import radioquiz2 from '../../img/projects_images/radioquiz/radioquiz2.jpeg';
 
 export const projectdata = [
   {
@@ -146,6 +149,65 @@ export const projectdata = [
     demo: 'https://tomkra.dev/',
   },
   {
+    id: 9,
+
+    name: 'X-Dream-Fit',
+    tags: ['All', 'Website', 'React', 'JavaScript', 'Node', 'SASS', 'CSS'],
+    text: 'Website for Fitness, Pilates Coach, and Freediving Instructor Susi Roth. The site is currently under development and aims to provide an overview of her entire range of services. For the CMS, I used Front-Aid so that Susi can easily edit the content herself. In the future, there will be a member area with personalised training plans. The website is built with React and Node.js. The design was created in Figma, and the layout was built with SASS.',
+    images: [
+      {
+        original: xdream,
+      },
+    ],
+    linkto: '',
+    github: 'https://github.com/tom-mate-o/X-Dream-Fit-Website',
+    demo: '',
+  },
+
+  {
+    id: 10,
+
+    name: 'Radio-OnAir-Quiz',
+    tags: ['All', 'Web-App', 'React', 'JavaScript', 'Node', 'SASS', 'CSS'],
+    text: 'A Live On-Air Quiz Interface for the Morning Show "Opstaan met Daan" on Radio Texel. The Host can easily navigate through the rounds and questions. He can see the correct answer and trigger a live Soundeffect if the answer was correct or wrong. The Host can also see the current score of the contestant. The App is built with React and Node.js. The the layout was built with SASS.',
+    images: [
+      {
+        original: radioquiz1,
+      },
+      {
+        original: radioquiz2,
+      },
+    ],
+    linkto: '',
+    github: 'https://github.com/tom-mate-o/radio_quiz_dashboard',
+    demo: '',
+  },
+
+  {
+    id: 7,
+
+    name: 'Todo-App',
+    tags: [
+      'All',
+      'React',
+      'Express',
+      'Mongo-DB',
+      'Web-App',
+      'JavaScript',
+      'Node',
+      'CSS',
+    ],
+    text: "A To-Do Web-App in Sticky Notes Style, which I created in React. The note data is stored in MongoDB. They can be posted, edited, and deleted there. I styled it using React's 'Styled Components' and CSS.",
+    images: [
+      {
+        original: imgtodo,
+      },
+    ],
+    linkto: '',
+    github: 'https://github.com/tom-mate-o/05-03-React-ToDo-App',
+    demo: '',
+  },
+  {
     id: 5,
 
     name: 'Lucia Kranawetter',
@@ -170,56 +232,11 @@ export const projectdata = [
     demo: 'https://tom-mate-o.github.io/portfoliolucy/',
   },
   {
-    id: 6,
-
-    name: 'Pokedex',
-    tags: ['All', 'Web-App', 'JavaScript', 'CSS'],
-    text: '...',
-    images: [
-      {
-        original: imgpokedex1,
-      },
-      {
-        original: imgpokedex2,
-      },
-      {
-        original: imgpokedex3,
-      },
-    ],
-    linkto: '',
-    github: 'https://github.com/tom-mate-o/Pokedex',
-    demo: 'https://tom-mate-o.github.io/Pokedex/',
-  },
-  {
-    id: 7,
-
-    name: 'Todo-App',
-    tags: [
-      'All',
-      'React',
-      'Express',
-      'Mongo-DB',
-      'Web-App',
-      'JavaScript',
-      'Node',
-      'CSS',
-    ],
-    text: '...',
-    images: [
-      {
-        original: imgtodo,
-      },
-    ],
-    linkto: '',
-    github: 'https://github.com/tom-mate-o/05-03-React-ToDo-App',
-    demo: '',
-  },
-  {
     id: 8,
 
     name: 'Planet Facts',
     tags: ['All', 'Website', 'React', 'JavaScript', 'Node', 'CSS'],
-    text: '...',
+    text: 'That was a Frontend Mentor challenge that I participated in. The task was to implement a Figma design sheet into code. I chose React and turned the page into a Single Page Application. I styled the components using Styled Components and CSS. The texts and images are loaded on demand from separate JavaScript files.',
     images: [
       {
         original: imgplanetfacts1,
@@ -234,5 +251,27 @@ export const projectdata = [
     linkto: '',
     github: 'https://tom-mate-o.github.io/Planet-Facts-React/',
     demo: 'https://tom-mate-o.github.io/Planet-Facts-React/',
+  },
+  {
+    id: 6,
+
+    name: 'Pokedex',
+    tags: ['All', 'Web-App', 'JavaScript', 'CSS'],
+    text: 'A Pokédex that I built using the PokéAPI. You can send a request to the API via the input field, in this case the Pokédex number, and the API returns the name of the Pokémon, an image, a short description, and the type and displays them.',
+
+    images: [
+      {
+        original: imgpokedex1,
+      },
+      {
+        original: imgpokedex2,
+      },
+      {
+        original: imgpokedex3,
+      },
+    ],
+    linkto: '',
+    github: 'https://github.com/tom-mate-o/Pokedex',
+    demo: 'https://tom-mate-o.github.io/Pokedex/',
   },
 ];
